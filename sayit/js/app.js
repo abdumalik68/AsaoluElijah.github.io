bc5059f9c6f14e80cc058ba3fe1e1421b1cda559
Asaolu Elijah <34748637+AsaoluElijah@users.noreply.github.com>
2020-01-07 11:38:59 +0100

    fixed repeating output

--- sayit/js/app.js
+++ sayit/js/app.js
@@ -16,8 +16,8 @@ function startRecording() {
     recognition.onresult = function(event) {
         console.log(event || "nothing here");
         var output = document.getElementById("output");
-       // output.innerHTML = "";
-
+          //  ...
+        output.innerHTML = output.innerHTML;
         for (var i = 0; i < event.results.length; i++) {
             output.innerHTML = output.innerHTML + event.results[i][0].transcript;
             textOutput = textOutput + event.results[i][0].transcript;
